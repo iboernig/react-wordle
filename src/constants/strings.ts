9ed5524fc951c3bae168f21be6c61d2285e62e40
iboernig@redhat.com <iboernig@redhat.com@che>
2022-10-29 16:11:49 +0000

    Translation part 1

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,13 +1,13 @@
 export const GAME_TITLE = process.env.REACT_APP_GAME_NAME!
 
-export const WIN_MESSAGES = ['Great Job!', 'Awesome', 'Well done!']
-export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
-export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters'
-export const WORD_NOT_FOUND_MESSAGE = 'Word not found'
+export const WIN_MESSAGES = ['Spitze', 'Klasse', 'Gut gemacht!']
+export const GAME_COPIED_MESSAGE = 'Spiel in Zwischenablage kopiert'
+export const NOT_ENOUGH_LETTERS_MESSAGE = 'Nicht genug Buchstaben'
+export const WORD_NOT_FOUND_MESSAGE = 'Wort nicht gefunden'
 export const HARD_MODE_ALERT_MESSAGE =
-  'Hard Mode can only be enabled at the start!'
+  'Den schwierigen Modus kann man nur zu Beginn einschalten!'
 export const HARD_MODE_DESCRIPTION =
-  'Any revealed hints must be used in subsequent guesses'
+  'JedeTipp muss in den nächsten Versuchen angewendet werden!'
 export const HIGH_CONTRAST_MODE_DESCRIPTION = 'For improved color vision'
 export const CORRECT_WORD_MESSAGE = (solution: string) =>
   `The word was ${solution}`
@@ -15,15 +15,15 @@ export const WRONG_SPOT_MESSAGE = (guess: string, position: number) =>
   `Must use ${guess} in position ${position}`
 export const NOT_CONTAINED_MESSAGE = (letter: string) =>
   `Guess must contain ${letter}`
-export const ENTER_TEXT = 'Enter'
-export const DELETE_TEXT = 'Delete'
-export const STATISTICS_TITLE = 'Statistics'
+export const ENTER_TEXT = 'Eingabe'
+export const DELETE_TEXT = 'Löschen'
+export const STATISTICS_TITLE = 'Statistik'
 export const GUESS_DISTRIBUTION_TEXT = 'Guess Distribution'
-export const NEW_WORD_TEXT = 'New word in'
-export const SHARE_TEXT = 'Share'
+export const NEW_WORD_TEXT = 'Neues Wort in'
+export const SHARE_TEXT = 'teile'
 export const SHARE_FAILURE_TEXT =
   'Unable to share the results. This feature is available only in secure contexts (HTTPS), in some or all supporting browsers.'
-export const MIGRATE_BUTTON_TEXT = 'Transfer'
+export const MIGRATE_BUTTON_TEXT = 'Übertrage'
 export const MIGRATE_DESCRIPTION_TEXT =
   'Click here to transfer your statistics to a new device.'
 export const TOTAL_TRIES_TEXT = 'Total tries'
