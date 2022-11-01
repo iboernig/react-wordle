e5a4c0cb883b3291039a2c791c9cce7a66fa83aa
iboernig@redhat.com <iboernig@redhat.com@che>
2022-11-01 14:40:30 +0000

    weitere Übersetzung

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -7,33 +7,33 @@ export const WORD_NOT_FOUND_MESSAGE = 'Wort nicht gefunden'
 export const HARD_MODE_ALERT_MESSAGE =
   'Den schwierigen Modus kann man nur zu Beginn einschalten!'
 export const HARD_MODE_DESCRIPTION =
-  'JedeTipp muss in den nächsten Versuchen angewendet werden!'
-export const HIGH_CONTRAST_MODE_DESCRIPTION = 'For improved color vision'
+  'Jeder mTipp muss in den nächsten Versuchen angewendet werden!'
+export const HIGH_CONTRAST_MODE_DESCRIPTION = 'Für bessere Farbdarstellung'
 export const CORRECT_WORD_MESSAGE = (solution: string) =>
-  `The word was ${solution}`
+  `Das Wort war ${solution}`
 export const WRONG_SPOT_MESSAGE = (guess: string, position: number) =>
-  `Must use ${guess} in position ${position}`
+  `Du musst ${guess} in der ${position} haben.`
 export const NOT_CONTAINED_MESSAGE = (letter: string) =>
-  `Guess must contain ${letter}`
+  `${letter} muss enthalten sein!`
 export const ENTER_TEXT = 'Eingabe'
 export const DELETE_TEXT = 'Löschen'
 export const STATISTICS_TITLE = 'Statistik'
-export const GUESS_DISTRIBUTION_TEXT = 'Guess Distribution'
+export const GUESS_DISTRIBUTION_TEXT = 'Vereilung der Versuche'
 export const NEW_WORD_TEXT = 'Neues Wort in'
-export const SHARE_TEXT = 'teile'
+export const SHARE_TEXT = 'Teile'
 export const SHARE_FAILURE_TEXT =
-  'Unable to share the results. This feature is available only in secure contexts (HTTPS), in some or all supporting browsers.'
+  'Kann die Ergebnisse leider nicht teilen.'
 export const MIGRATE_BUTTON_TEXT = 'Übertrage'
 export const MIGRATE_DESCRIPTION_TEXT =
-  'Click here to transfer your statistics to a new device.'
-export const TOTAL_TRIES_TEXT = 'Total tries'
-export const SUCCESS_RATE_TEXT = 'Success rate'
-export const CURRENT_STREAK_TEXT = 'Current streak'
-export const BEST_STREAK_TEXT = 'Best streak'
+  'Hiermit kannst du die Statistiken auf einderes Gerät übertragen.'
+export const TOTAL_TRIES_TEXT = 'Gesamtzahl der Versuche'
+export const SUCCESS_RATE_TEXT = 'Erfolgsrate'
+export const CURRENT_STREAK_TEXT = 'Aktuelles Ergebnis'
+export const BEST_STREAK_TEXT = 'Bestes Ergebnis'
 export const DISCOURAGE_INAPP_BROWSER_TEXT =
-  "You are using an embedded browser and may experience problems sharing or saving your results. We encourage you rather to use your device's default browser."
+  "Versuche bitte einen anderen Browser."
 
-export const DATEPICKER_TITLE = 'Choose a past date'
-export const DATEPICKER_CHOOSE_TEXT = 'Choose'
-export const DATEPICKER_TODAY_TEXT = 'today'
-export const ARCHIVE_GAMEDATE_TEXT = 'Game date'
+export const DATEPICKER_TITLE = 'Suche ein vergangenes Datum'
+export const DATEPICKER_CHOOSE_TEXT = 'Wähle'
+export const DATEPICKER_TODAY_TEXT = 'Heute'
+export const ARCHIVE_GAMEDATE_TEXT = 'Spieldatum'
